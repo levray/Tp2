fdd687dadf59f5e5a0c1e9412d8fe3fe60031d29
LEVRAY Julien <LEVRAY@stex.lyon.aeroport.fr>
2017-11-24 11:43:17 +0100

    Initial commit

---
+++ app/src/main/java/fr/sciencesu/tp2/Tp2.java
@@ -0,0 +1,13 @@
+package fr.sciencesu.tp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tp2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tp2);
+    }
+}
